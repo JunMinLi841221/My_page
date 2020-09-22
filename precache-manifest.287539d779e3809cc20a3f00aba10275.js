0b904ab046bf5b49f032c8918081e6b3ef15c783
JunMinLi841221 <jli58@mail.sfsu.edu>
2020-09-21 21:05:43 -0700

    Updates

---
+++ precache-manifest.287539d779e3809cc20a3f00aba10275.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d843661a42008a0774a6289fd7807dc",
+    "url": "/My_page/index.html"
+  },
+  {
+    "revision": "5c1a083a8308f812992f",
+    "url": "/My_page/static/css/main.220c0139.chunk.css"
+  },
+  {
+    "revision": "5536ae86322a459240ff",
+    "url": "/My_page/static/js/2.8dffe265.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/My_page/static/js/2.8dffe265.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c1a083a8308f812992f",
+    "url": "/My_page/static/js/main.d88e7104.chunk.js"
+  },
+  {
+    "revision": "9df4356f645a53e83059",
+    "url": "/My_page/static/js/runtime-main.cd38441d.js"
+  },
+  {
+    "revision": "e260d478f8338b89cb308aa0ca98551c",
+    "url": "/My_page/static/media/ACCESS.e260d478.png"
+  },
+  {
+    "revision": "97932ef343cf78174b215a11fb3ea347",
+    "url": "/My_page/static/media/After_show.97932ef3.JPG"
+  },
+  {
+    "revision": "bccb6ed113f1151c22b15b9c17871f36",
+    "url": "/My_page/static/media/Cat.bccb6ed1.JPG"
+  },
+  {
+    "revision": "e13ed6c7fb2019e86d04a50c5a851d8a",
+    "url": "/My_page/static/media/Dance.e13ed6c7.JPG"
+  },
+  {
+    "revision": "4f4fe0cf8fab8772a43fca4a6a84eecb",
+    "url": "/My_page/static/media/GD.4f4fe0cf.png"
+  },
+  {
+    "revision": "824bdb42488b4e7a8ccc3a823356117c",
+    "url": "/My_page/static/media/Kiss.824bdb42.JPG"
+  },
+  {
+    "revision": "2222a2e6c6c2de91b2ae964a983f13dc",
+    "url": "/My_page/static/media/Landscape.2222a2e6.JPG"
+  },
+  {
+    "revision": "e313dcd17557c374e080c6bb4875678f",
+    "url": "/My_page/static/media/Me.e313dcd1.png"
+  },
+  {
+    "revision": "4f16d430da3e68910e749a7b3ac5c397",
+    "url": "/My_page/static/media/Momentum.4f16d430.png"
+  },
+  {
+    "revision": "869b93a9631b94f5607e5f43a9a74a78",
+    "url": "/My_page/static/media/Real_modle.869b93a9.JPG"
+  },
+  {
+    "revision": "ddca869b46f5f0d75a8a2c3007bbf64a",
+    "url": "/My_page/static/media/Yawning.ddca869b.JPG"
+  }
+]);
